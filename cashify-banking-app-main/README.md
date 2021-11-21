abdc05f93bc0ac79afff3cf69c5e348246cf5ede
Mohamed Sihan <39284618+Mshihan@users.noreply.github.com>
2021-11-21 19:30:19 +0530

    tweaked hamburger menu for responsivenes

---
+++ cashify-banking-app-main/README.md
@@ -0,0 +1,4 @@
+# cashify-banking-app
+
+Initial commit with all basic functionalities.
+See this project live in github pages https://nivethan-ar.github.io/cashify-banking-app/
---
+++ cashify-banking-app-main/js/login.js
@@ -0,0 +1,43 @@
+// "use strict";
+
+// function submitForm(event) {
+//   event.preventDefault();
+//   var username = document.getElementById("username").value;
+//   var password = document.getElementById("password").value;
+//   // check the credentials with database
+//   var hasError = false;
+//   if (hasError) {
+//     document.getElementById("errorText").innerText =
+//       "Please check the credentials and try again.";
+//   } else {
+//     document.getElementById("errorText").innerText = "";
+//     //submit to the database
+
+//     document.getElementById("password").value = "";
+//     document.getElementById("username").value = "";
+
+//     alert("success");
+//   }
+// }
+
+function registrationRedirect() {
+  window.location.href = "signup.html";
+}
+
+function showPassword() {
+  var passwordTextBox = document.getElementById("password");
+  var passwordCheckBox = document.getElementById("showPasswordCheckBox");
+  if (passwordTextBox.type == "password") {
+    console.log("Executing");
+    passwordTextBox.type = "text";
+    passwordCheckBox.checked = true;
+  } else {
+    passwordTextBox.type = "password";
+    passwordCheckBox.checked = false;
+  }
+}
+
+//Redicrect to Home
+function backToHome() {
+  window.location.href = "../index.html";
+}
